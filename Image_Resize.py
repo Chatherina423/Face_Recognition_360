870f9f3c95ec55c92e742c116aaef829cd95ba02
Chatherina423 <ysh9473@naver.com>
2021-03-25 23:35:15 +0900

    Resized completed

--- Image_Resize.py
+++ Image_Resize.py
@@ -1,6 +1,19 @@
 from PIL import Image
+import glob
+import os
 
-img = Image.open('/Users/suhyeonyoo/Downloads/archive/PNGImages/FudanPed00001.png')
+# img = Image.open('/Users/suhyeonyoo/Downloads/archive/PNGImages/FudanPed00001.png')
+# # #
+# # # img_resize = img.resize((300,300), Image.BICUBIC)
+# # # img_resize.save('example.jpg')
 
-img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
-img_resize.save('example.jpg')
+
+files = glob.glob('/Users/suhyeonyoo/Downloads/archive/PNGImages/*.png')
+print(files)
+
+for f in files:
+    img = Image.open(f)
+    img_resize = img.resize((300,300), Image.BICUBIC)
+    directory, name = os.path.split(f)
+    print(directory, name)
+    img_resize.save('/Users/suhyeonyoo/Desktop/Face_Recognition_360/Resized_300/'+ name)
